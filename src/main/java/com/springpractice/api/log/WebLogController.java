decda9c1fa816ed9a91f22fb66bf7ce3f62f7e4a
awesomejang <uljang1995@gmail.com>
2024-08-03 14:32:44 +0900

    AbstractRequestLoggingFilter config

--- src/main/java/com/springpractice/api/log/WebLogController.java
+++ src/main/java/com/springpractice/api/log/WebLogController.java
@@ -1,5 +1,6 @@
 package com.springpractice.api.log;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/log")
 public class WebLogController {
 
+    @GetMapping("")
     public String log() {
         return "Logging";
     }
--- src/main/java/com/springpractice/common/CommonConfiguration.java
+++ src/main/java/com/springpractice/common/CommonConfiguration.java
@@ -2,11 +2,13 @@ package com.springpractice.common;
 
 import com.springpractice.filter.WebLogFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfiguration {
 
+    @Bean
     public FilterRegistrationBean<WebLogFilter> webLogFilterRegistrationBean() {
         FilterRegistrationBean<WebLogFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new WebLogFilter());
