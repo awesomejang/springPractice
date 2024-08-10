a8575bdaca85b6304cde0806a7e020871e4fa99b
awesomejang <uljang1995@gmail.com>
2024-08-10 15:03:41 +0900

    HttpServletRequestWrapper 활용

--- src/main/java/com/springpractice/filter/CustomRequestLoggingFilter.java
+++ src/main/java/com/springpractice/filter/CustomRequestLoggingFilter.java
@@ -2,18 +2,33 @@ package com.springpractice.filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class CustomRequestLoggingFilter extends OncePerRequestFilter {
+// Http Request Body에 접근하면 정책에 따라 body 내용이 사라지기 때문에 wrapping 작업이 필요
+public class CustomRequestLoggingFilter extends HttpServletRequestWrapper {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    private final byte[] requestBody;
 
+    public CustomRequestLoggingFilter(HttpServletRequest request) throws IOException {
+        super(request);
+        requestBody = request.getInputStream().readAllBytes();
+    }
 
-
+    public ServletInputStream getInputStream() {
+        return new CustomRequestLoggingFilter()
     }
 }
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//
+//
+//
+//    }
+//}
