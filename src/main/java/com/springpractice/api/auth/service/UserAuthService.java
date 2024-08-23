98b9da1d8c01dc30c06166faf51542a4ddeee247
awesomejang <uljang1995@gmail.com>
2024-08-24 00:12:20 +0900

    [JWT+REDIS] 인증 정보 관리 처리 로직 추가

---
+++ src/main/java/com/springpractice/api/auth/service/UserAuthService.java
@@ -0,0 +1,12 @@
+package com.springpractice.api.auth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAuthService {
+
+    public checkValidUser(String clientId, String clientSecret) {
+
+        return true;
+    }
+}
