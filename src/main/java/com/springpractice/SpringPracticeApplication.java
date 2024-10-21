de5c79cb1c986f0c5d75daefabc57194b30755e1
awesomejang <uljang1995@gmail.com>
2024-10-22 00:22:35 +0900

    [ASYNC] 비동기 처리 관련 초기설정

--- src/main/java/com/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/springpractice/SpringPracticeApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
 @EnableAspectJAutoProxy
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
+@EnableAsync
+@SpringBootApplication
 public class SpringPracticeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/springpractice/api/common/service/AsyncService.java
@@ -0,0 +1,22 @@
+package com.springpractice.api.common.service;
+
+import com.springpractice.common.advice.RestExceptionControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AsyncService {
+
+    private final Logger logger = LoggerFactory.getLogger(AsyncService.class);
+
+    public void asyncMethod() {
+        logger.debug("Async Method Start");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            logger.error("Async Method Error", e);
+        }
+        logger.debug("Async Method End");
+    }
+}
