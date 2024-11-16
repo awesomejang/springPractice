1f18468782b1a5119b7bdf4c9d5704b64acf4e99
awesomejang <uljang1995@gmail.com>
2024-11-16 23:58:02 +0900

    [Async] TaskExecutor 추가 작업

---
+++ src/main/java/com/springpractice/common/AsyncExecutorConfig.java
@@ -0,0 +1,11 @@
+package com.springpractice.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AsyncExecutorConfig {
+
+    @Bean(name = "asyncExecutor")
+
+}
